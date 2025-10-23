44db8b6eddc2c38a59ce65abbc0302a4c8fd3b17
Naviya-C <naviyac491@gmail.com>
2025-10-23 11:20:03 +0530

    add middleware to fastapi to talk with front-end

--- Deploy/Back-End/main.py
+++ Deploy/Back-End/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from model import load_model, predict
 from schemas import PredictRequest, PredictionResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 model = load_model()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://127.0.0.1:5500"],
+    allow_credentials = True,
+    allow_methods = [""],
+    allow_header = [""],
+)
 
 @app.post("/predict", response_model = PredictionResponse)
 def make_prediction(request: PredictRequest):
